36e005552f40bf6379e8d711b2d6155fa2f83759
Christopher Schnick <crschnick@xpipe.io>
2023-05-28 00:18:02 +0200

    Update download page

--- src/components/ActionDownload/index.tsx
+++ src/components/ActionDownload/index.tsx
@@ -4,6 +4,7 @@ import React from "react"
 // @ts-ignore
 import BashScript from "./bash-script.mdx";
 import Homebrew from "./homebrew.mdx";
+import Choco from "./choco.mdx";
 import MDXContent from '@theme/MDXContent';
 
 export const ActionDownload = () => (
@@ -19,19 +20,23 @@ export const ActionDownload = () => (
                 >
                     <a className={styles.card__link}
                        href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-installer-windows-x86_64.msi">
-                        &gt;&nbsp;&nbsp;Windows .msi installer (x86_64)
+                        &gt;&nbsp;&nbsp;Windows .msi installer (x86-64)
                     </a>
                     <a className={styles.card__link}
                        href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-installer-linux-x86_64.deb">
-                        &gt;&nbsp;&nbsp;Linux .deb installer (x86_64)
+                        &gt;&nbsp;&nbsp;Linux .deb installer (x86-64)
                     </a>
                     <a className={styles.card__link}
                        href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-installer-linux-x86_64.rpm">
-                        &gt;&nbsp;&nbsp;Linux .rpm installer (x86_64)
+                        &gt;&nbsp;&nbsp;Linux .rpm installer (x86-64)
                     </a>
                     <a className={styles.card__link}
                        href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-installer-macos-x86_64.pkg">
-                        &gt;&nbsp;&nbsp;MacOS .pkg installer (x86_64)
+                        &gt;&nbsp;&nbsp;MacOS .pkg installer (x86-64)
+                    </a>
+                    <a className={styles.card__link}
+                       href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-installer-macos-arm64.pkg">
+                        &gt;&nbsp;&nbsp;MacOS .pkg installer (ARM 64)
                     </a>
                     Download and install X-Pipe automatically with the following script:
                     <div>
@@ -57,15 +62,19 @@ export const ActionDownload = () => (
                 >
                     <a className={styles.card__link}
                        href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-portable-windows-x86_64.zip">
-                        &gt;&nbsp;&nbsp;Windows .zip archive (x86_64)
+                        &gt;&nbsp;&nbsp;Windows .zip archive (x86-64)
                     </a>
                     <a className={styles.card__link}
                        href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-portable-linux-x86_64.tar.gz">
-                        &gt;&nbsp;&nbsp;Linux .tar.gz archive (x86_64)
+                        &gt;&nbsp;&nbsp;Linux .tar.gz archive (x86-64)
                     </a>
                     <a className={styles.card__link}
                        href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-portable-macos-x86_64.dmg">
-                        &gt;&nbsp;&nbsp;MacOS .dmg archive (x86_64)
+                        &gt;&nbsp;&nbsp;MacOS .dmg archive (x86-64)
+                    </a>
+                    <a className={styles.card__link}
+                       href="https://github.com/xpipe-io/xpipe/releases/latest/download/xpipe-portable-macos-arm64.dmg">
+                        &gt;&nbsp;&nbsp;MacOS .dmg archive (ARM 64)
                     </a>
                 </ActionCard>
                 <ActionCard
@@ -79,6 +88,10 @@ export const ActionDownload = () => (
                         <h4>Homebrew</h4>
                         <MDXContent>
                             <Homebrew/>
+                        </MDXContent>
+                        <h4>Chocolatey</h4>
+                        <MDXContent>
+                            <Choco/>
                         </MDXContent>
                     </div>
                 </ActionCard>
